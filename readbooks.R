cd21a9cc1d9b7faa89976f20d471f3b4521d8ecc
davidbrae <davidbrae@gmail.com>
2020-10-30 11:36:13 -0400

    fix month var

--- readbooks.R
+++ readbooks.R
@@ -13,3 +13,9 @@ books %>%
   group_by(Year,Gender) %>%
   summarize(n=n()) %>%
   pivot_wider(names_from = Gender,values_from=n)
+
+
+books %>%
+  filter(Year>2012) %>%
+  group_by(Month) %>%
+  summarize(n())
